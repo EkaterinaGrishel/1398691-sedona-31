3a40f4556078530b18865f24370c4a82dd70d9f8
EkaterinaGrishel <katesobol88@gmail.com>
2021-07-18 22:47:50 +0300

    Выполнила оживление сайта

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+const searchButton = document.querySelector(".search-button");
+const popup = document.querySelector(".modal");
+const form = popup.querySelector("form");
+const arrivalDate = popup.querySelector("[name=arrival-date]");
+const chekOutlDate = popup.querySelector("[name=check-out-date]");
+const adults = popup.querySelector("[name=adults]");
+const children = popup.querySelector("[name=children]");
+
+searchButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+
+    arrivalDate.focus();
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!arrivalDate.value || !chekOutlDate.value || !adults.value || !children.value) {
+        evt.preventDefault();
+    }
+});
