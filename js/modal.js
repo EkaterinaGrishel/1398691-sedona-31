3a2149de3d00eacc86d3c5928d9d31226fb0990a
EkaterinaGrishel <katesobol88@gmail.com>
2021-07-19 00:52:59 +0300

    Внесла поправки в проект

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ const searchButton = document.querySelector(".search-button");
 const popup = document.querySelector(".modal");
 const form = popup.querySelector("form");
 const arrivalDate = popup.querySelector("[name=arrival-date]");
-const chekOutlDate = popup.querySelector("[name=check-out-date]");
+const chekOutDate = popup.querySelector("[name=check-out-date]");
 const adults = popup.querySelector("[name=adults]");
 const children = popup.querySelector("[name=children]");
 
@@ -14,7 +14,7 @@ searchButton.addEventListener("click", function(evt) {
 });
 
 form.addEventListener("submit", function(evt) {
-    if (!arrivalDate.value || !chekOutlDate.value || !adults.value || !children.value) {
+    if (!arrivalDate.value || !chekOutDate.value || !adults.value || !children.value) {
         evt.preventDefault();
     }
 });
